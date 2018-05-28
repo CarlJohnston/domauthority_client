a9b38119987d7aa6c324ab172dffb4863610a92a
Carl Johnston <carljohnston28@gmail.com>
2018-05-27 18:15:51 -0700

    Add foundation-sites to client-side

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import jQuery from 'jquery';
+import 'foundation-sites/dist/css/foundation.min.css'
+
 import './App.css';
 
 import Layout from '../Layout/Layout';
+
+window.$ = window.jQuery = require('jquery')
+const Foundation = require('foundation-sites');
 
 class App extends Component {
   render() {
