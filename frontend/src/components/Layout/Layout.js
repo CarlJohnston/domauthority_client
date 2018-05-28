589f5919e6fb97f772b312a6ae4269d1e4de8b68
Carl Johnston <carljohnston28@gmail.com>
2018-05-27 18:23:29 -0700

    Basic layout

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -5,6 +5,40 @@ class Layout extends Component {
   render() {
     return (
       <div>
+        <div class="grid-container">
+          <div class="top-bar">
+            <div class="top-bar-left">
+              <ul class="dropdown menu" data-dropdown-menu>
+                <li class="menu-text">Simplicify</li>
+                <li><a href="#">One</a></li>
+                <li><a href="#">Two</a></li>
+                <li><a href="#">Three</a></li>
+              </ul>
+            </div>
+            <div class="top-bar-right">
+              <ul class="menu">
+                <li><input type="search" placeholder="Search" /></li>
+                <li><button type="button" class="button">Search</button></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid-container">
+          <div class="grid-x align-justify align-middle">
+            <div class="small-12 medium-shrink cell">
+              <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+
+            <div class="small-12 medium-shrink cell">
+              <span>Copyright 2018</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
