54898de94dbab686d8cd3257a4beed94f19ccee3
Carl Johnston <carljohnston28@gmail.com>
2018-05-27 17:09:24 -0700

    Add basic layout file to frontend

---
+++ frontend/src/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Layout;
