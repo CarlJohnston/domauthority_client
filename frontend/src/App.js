575510d61206d3c865e93c6a64cf4edd1459a7a9
Carl Johnston <carljohnston28@gmail.com>
2018-05-27 17:11:25 -0700

    Remove unnecessary JS file extension on import for Layout import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import Layout from './Layout.js';
+import Layout from './Layout';
 
 class App extends Component {
   render() {
