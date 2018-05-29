3fb1f91cf99f19cb810731bb6ee0ff21b5ca2cf9
Carl Johnston <carljohnston28@gmail.com>
2018-05-28 18:40:05 -0700

    Add forgotten password initial component

---
+++ frontend/src/components/Forgot/Forgot.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+class Forgot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Forgot Password</h1>
+      </div>
+    );
+  }
+}
+
+export default Forgot;
---
+++ frontend/src/tests/components/Forgot/Forgot.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Forgot from '../../../components/Forgot/Forgot';
+
+it('renders without crashing', () => {
+  const component = TestRenderer.create(
+    <Router>
+      <Forgot />
+    </Router>
+  );
+  component.unmount();
+});
