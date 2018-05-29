21041f9971576311d4d56b3206645f7fd4213ec1
Carl Johnston <carljohnston28@gmail.com>
2018-05-29 15:13:55 -0700

    Skip confirmation emails

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
+
   include DeviseTokenAuth::Concerns::User
+
+  before_create :skip_confirmation!
 end
