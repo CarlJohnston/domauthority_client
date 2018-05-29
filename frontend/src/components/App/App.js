badae6363b53c9136dc9ff8dc0c2292bc5d343cd
Carl Johnston <carljohnston28@gmail.com>
2018-05-28 20:20:18 -0700

    Remove unnecessary Foundation variable assignment

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import jQuery from 'jquery';
 import 'foundation-sites/dist/css/foundation.min.css'
 
 import './App.css';
 
 import Layout from '../Layout/Layout';
 
-window.$ = window.jQuery = require('jquery')
-const Foundation = require('foundation-sites');
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+require('foundation-sites');
 
 class App extends Component {
   render() {
