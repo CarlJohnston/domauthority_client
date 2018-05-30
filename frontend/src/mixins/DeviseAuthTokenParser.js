d1a80e42a2728844ec952cd362ad3a3de44ba2bb
Carl Johnston <carljohnston28@gmail.com>
2018-05-29 17:53:31 -0700

    Use === instead of == in DeviseAuthToken parser

--- frontend/src/mixins/DeviseAuthTokenParser.js
+++ frontend/src/mixins/DeviseAuthTokenParser.js
@@ -16,7 +16,7 @@ class DeviseAuthTokenParser extends Parser {
     var errors = [];
 
     if ('errors' in this.data) {
-      if (typeof this.data.errors == 'object') {
+      if (typeof this.data.errors === 'object') {
         if ('full_messages' in this.data.errors) {
           this.data.errors.full_messages.forEach((error) => {
             errors.push(error);
@@ -41,7 +41,7 @@ class DeviseAuthTokenParser extends Parser {
     var status = '';
 
     if ('errors' in this.data &&
-       this.data.errors.length != 0) {
+       this.data.errors.length !== 0) {
       status = STATUS.error;
     } else {
       status = STATUS.success;
