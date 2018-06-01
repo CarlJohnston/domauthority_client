40fc583666a6594a633529f33e9f45f9b9a1c3f2
Carl Johnston <carljohnston28@gmail.com>
2018-05-31 19:43:12 -0700

    Initialize client side application with token from localStorage if present else empty values

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -11,14 +11,31 @@ import Layout from '../Layout/Layout';
 
 import CurrentUser from '../../contexts/User';
 
+import TOKEN from '../../helpers/Token';
+
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 require('foundation-sites');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    var token = localStorage.getItem(TOKEN.authentication.key);
+    this.state = {
+      currentUser: {
+        uid: token ? token.uid : null,
+        name: token ? token.name : null,
+        username: token ? token.username : null,
+        accessToken: token ? token.accessToken : null,
+        client: token ? token.client : null,
+      },
+    };
+  }
+
   render() {
     return (
-      <CurrentUser.Provider value={{name: "Name", username: "myUsername"}}>
+      <CurrentUser.Provider value={this.state.currentUser}>
         <Router>
           <Route to='/' component={Layout} />
         </Router>
