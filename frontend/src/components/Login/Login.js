079dc1527a57bb021665114d6781d4f874f46a79
Carl Johnston <carljohnston28@gmail.com>
2018-06-02 14:40:34 -0700

    Move login submission logic to separate method

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -31,65 +31,70 @@ class Login extends Component {
     this.$form.foundation();
 
     this.$form.on('formvalid.zf.abide', function (e) {
-      var email = this.$form.find('#email').val();
-      var password = this.$form.find('#password').val();
-      var request = new Request('/authenticate/sign_in', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
-      var headers;
-      fetch(request)
-        .then((response) => {
-          headers = response.headers;
-
-          return response.json();
-        }).then((body) => {
-          this.deviseAuthTokenParser.setData(body);
-          var status = this.deviseAuthTokenParser.getStatus();
-          var messages = {};
-          if (status == STATUS.success) {
-            messages['Successfully authenticated.'] = STATUS.success;
-          } else {
-            var errors = this.deviseAuthTokenParser.getErrors();
-            errors.forEach((error) => {
-              messages[error] = STATUS.error;
-            });
-          }
-
-          Object.entries(messages).forEach((message) => {
-            PNotify.alert({
-              text: message[0],
-              type: message[1],
-              delay: 2000,
-            });
-          });
-
-          if (status == STATUS.success) {
-            var data = body.data;
-            headers.forEach((value, key) => {
-              headers[key] = value;
-            });
-            var token = {
-              uid: data.uid,
-              name: data.name,
-              username: data.username,
-              accessToken: headers['access-token'],
-              client: headers.client,
-            };
-            localStorage.setItem(TOKEN.authentication.key, JSON.stringify(token));
-
-            this.props.setCurrentUser(token);
-
-            this.props.history.push('/');
-          }
-        });
+      this.validLoginFormSubmit(e);
     }.bind(this));
+  }
+
+  validLoginFormSubmit(e) {
+    var email = this.$form.find('#email').val();
+    var password = this.$form.find('#password').val();
+    var request = new Request('/authenticate/sign_in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    var headers;
+    fetch(request)
+      .then((response) => {
+        headers = response.headers;
+
+        return response.json();
+      }).then((body) => {
+        this.deviseAuthTokenParser.setData(body);
+        var status = this.deviseAuthTokenParser.getStatus();
+        var messages = {};
+        if (status == STATUS.success) {
+          messages['Successfully authenticated.'] = STATUS.success;
+        } else {
+          var errors = this.deviseAuthTokenParser.getErrors();
+          errors.forEach((error) => {
+            messages[error] = STATUS.error;
+          });
+        }
+
+        Object.entries(messages).forEach((message) => {
+          PNotify.alert({
+            text: message[0],
+            type: message[1],
+            delay: 2000,
+          });
+        });
+
+        if (status == STATUS.success) {
+          var data = body.data;
+          headers.forEach((value, key) => {
+            headers[key] = value;
+          });
+          var token = {
+            uid: data.uid,
+            name: data.name,
+            username: data.username,
+            accessToken: headers['access-token'],
+            client: headers.client,
+          };
+          localStorage.setItem(TOKEN.authentication.key, JSON.stringify(token));
+
+          this.props.setCurrentUser(token);
+
+          this.props.history.push('/');
+        }
+      });
   }
 
   loginFormSubmit(e) {
