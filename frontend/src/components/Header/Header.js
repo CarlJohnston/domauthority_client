97b8d57423fb9f55076b3c74bda12ac7fd7168ef
Carl Johnston <carljohnston28@gmail.com>
2018-06-02 14:18:22 -0700

    Pull Header out to separate component

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import withCurrentUser from '../hocs/withCurrentUser';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="dropdown menu" data-dropdown-menu>
+              <li><Link to='/'>Simplicify</Link></li>
+              <li><Link to='/topics'>Topics</Link></li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <ul className="menu">
+              <li><input type="search" placeholder="Search" /></li>
+              <li><button type="button" className="button">Search</button></li>
+
+              {this.props.currentUser.uid &&
+               <li><Link to={`/users/${this.props.currentUser.username}`}>@{this.props.currentUser.username}</Link></li>
+              }
+              {this.props.currentUser.uid &&
+               <li><a href="#">Logout</a></li>
+              }
+
+              {!this.props.currentUser.uid &&
+               <li><Link to='/login'>Login</Link></li>
+              }
+              {!this.props.currentUser.uid &&
+               <li><Link to='/register'>Register</Link></li>
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withCurrentUser(Header);
--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
 import Forgot from '../Forgot/Forgot';
@@ -15,36 +16,7 @@ class Layout extends Component {
   render() {
     return (
       <div>
-        <div className="grid-container">
-          <div className="top-bar">
-            <div className="top-bar-left">
-              <ul className="dropdown menu" data-dropdown-menu>
-                <li><Link to='/'>Simplicify</Link></li>
-                <li><Link to='/topics'>Topics</Link></li>
-              </ul>
-            </div>
-            <div className="top-bar-right">
-              <ul className="menu">
-                <li><input type="search" placeholder="Search" /></li>
-                <li><button type="button" className="button">Search</button></li>
-
-                {this.props.currentUser.uid &&
-                 <li><Link to={`/users/${this.props.currentUser.username}`}>@{this.props.currentUser.username}</Link></li>
-                }
-                {this.props.currentUser.uid &&
-                 <li><a href="#">Logout</a></li>
-                }
-
-                {!this.props.currentUser.uid &&
-                 <li><Link to='/login'>Login</Link></li>
-                }
-                {!this.props.currentUser.uid &&
-                 <li><Link to='/register'>Register</Link></li>
-                }
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Header />
 
         <div className="grid-container">
           <Route exact path='/' component={Home} />
