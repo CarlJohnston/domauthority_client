d7b4916ac419e29d1feff99320e63649ecc8d757
Carl Johnston <carljohnston28@gmail.com>
2018-06-02 18:17:42 -0700

    Improve functionality of localStorage mock for Jest tests

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,6 +1,23 @@
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
 };
-global.localStorage = localStorageMock;
+
+global.localStorage = new LocalStorageMock;
