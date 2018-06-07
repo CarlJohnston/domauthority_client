2013b62926df007cbc3aaf03bcbbc65c7d27f389
Carl Johnston <carljohnston28@gmail.com>
2018-06-07 07:53:52 -0700

    Add basic Settings component for client-side

---
+++ frontend/src/components/Settings/Settings.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import CurrentUserContext from 'contexts/CurrentUserContext';
+
+class Settings extends Component {
+  render() {
+    return (
+        <div>
+        </div>
+    );
+  }
+}
+
+export default Settings;
