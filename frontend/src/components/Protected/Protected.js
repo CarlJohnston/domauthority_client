522b1833d2567e2e0593258ef10124a9f2045ff4
Carl Johnston <carljohnston28@gmail.com>
2018-06-07 13:20:08 -0700

    Pseudocode for token validation on react-router shifts

--- frontend/src/components/Protected/Protected.js
+++ frontend/src/components/Protected/Protected.js
@@ -6,6 +6,23 @@ class Protected extends Component {
   constructor(props) {
     super(props);
 
+    // TODO token validation
+    // if localStorage token
+    //   check token via AJAX
+    //   if token valid
+    //     set new localStorage token
+    //     continue
+    //   else
+    //     remove localStorage token
+    //     if history > 1
+    //       display popup
+    //     else
+    //       redirect to login
+    // else
+    //    if history > 1
+    //        display popup
+    //    else
+    //        redirect to login
     if (!this.props.currentUser.uid) {
       this.props.history.push('/login');
     }
