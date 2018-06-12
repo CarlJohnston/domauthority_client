203cb36d44b0eec3a2b9e81df334d01dfd81e35a
Carl Johnston <carljohnston28@gmail.com>
2018-06-12 15:01:25 -0700

    Add LoginResponse#getToken

--- frontend/src/tests/mixins/LoginResponse.test.js
+++ frontend/src/tests/mixins/LoginResponse.test.js
@@ -1,6 +1,16 @@
 import LoginResponse from 'mixins/LoginResponse';
 import STATUS from 'configs/Status';
 
+const VALID_CURRENT_USER_DATA = {
+  uid: 1,
+  name: 'name',
+  username: 'username',
+};
+const VALID_CURRENT_USER_HEADERS = {
+  accessToken: 'access',
+  client: 'client',
+};
+
 describe('register response', () => {
   var options = {
     messages: {
@@ -34,6 +44,84 @@ describe('register response', () => {
     var expectedMessages = {};
     expectedMessages[options.messages.success] = STATUS.success;
     expect(messages).toEqual(expectedMessages);
+  });
 
+  it('fetches token from response', () => {
+    // valid data
+    var data = {
+      body: {
+        data: VALID_CURRENT_USER_DATA,
+      },
+      headers: new Headers({
+        'access-token': VALID_CURRENT_USER_HEADERS.accessToken,
+        client: VALID_CURRENT_USER_HEADERS.client,
+      }),
+    };
+    response.set(data);
+    var token = response.getToken();
+    expect(token).toEqual(Object.assign(VALID_CURRENT_USER_DATA,
+                                        VALID_CURRENT_USER_HEADERS));
+    // missing headers
+    data = {
+      body: {
+        data: VALID_CURRENT_USER_DATA,
+      },
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+    // wrong header type
+    data = {
+      body: {
+        data: VALID_CURRENT_USER_DATA,
+      },
+      headers: 'string',
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+    // missing body data
+    data = {
+      body: {},
+      headers: new Headers(VALID_CURRENT_USER_HEADERS),
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+
+    // wrong body data type
+    // missing body data
+    data = {
+      body: 'string',
+      headers: new Headers(VALID_CURRENT_USER_HEADERS),
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+    // no body data or headers
+    response.set({});
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+    // missing body
+    data = {
+      headers: new Headers(VALID_CURRENT_USER_HEADERS),
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
+
+    // wrong body type
+    data = {
+      body: 'string',
+      headers: new Headers(VALID_CURRENT_USER_HEADERS),
+    };
+    response.set(data);
+    token = response.getToken();
+    expect(token).toEqual(null);
   });
 });
