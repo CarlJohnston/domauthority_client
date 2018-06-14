ba438abd416d075c663b6da419f38408a7091f71
Carl Johnston <carljohnston28@gmail.com>
2018-06-14 15:28:34 -0700

    Add Footer css

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Footer.css';
+
 import withCurrentUser from 'components/hocs/withCurrentUser';
 
 class Footer extends Component {
   render() {
     return (
-      <div className="grid-container">
-        <div className="grid-x align-justify align-middle">
-          <div className="small-12 medium-shrink cell">
-            <ul className="menu">
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/about'>About</Link></li>
-              <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-          </div>
+      <div class="footer-container">
+        <div className="grid-container">
+          <div className="grid-x align-justify align-middle">
+            <div className="small-12 medium-shrink cell">
+              <ul className="menu">
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+              </ul>
+            </div>
 
-          <div className="small-12 medium-shrink cell">
-            <span>Copyright {(new Date().getFullYear())}</span>
+            <div className="small-12 medium-shrink cell">
+              <span>Copyright {(new Date().getFullYear())}</span>
+            </div>
           </div>
         </div>
       </div>
