b332411dfa5ccd905c19de1ecdb308990ced4ea7
Carl Johnston <carljohnston28@gmail.com>
2018-06-16 13:02:04 -0700

    Add create for Sites under current User
    What and why:
      => Site should the relevant relationships for the current user as well

--- backend/app/controllers/users/current/sites_controller.rb
+++ backend/app/controllers/users/current/sites_controller.rb
@@ -1,26 +1,72 @@
+require 'uri'
+
 class Users::Current::SitesController < ApplicationController
   before_action :authenticate_user!
 
   # GET users/current/sites
   def index
-    @sites = Site.all
+    @sites = Site.where(id: UserSite.where(user_id: current_user.id).pluck(:site_id))
 
     render json: @sites
   end
 
   # POST users/current/sites
   def create
-    @site = Site.new(site_params)
+    if params.has_key?(:site) && params[:site].has_key?(:url)
+      @previous_site = Site.find_by(url: params[:site][:url])
 
-    if @site.save
-      render json: @site, status: :created, location: @site
+      if params[:site].has_key?(:title)
+        title = params[:site][:title]
+      else
+        begin
+          title = URI(params[:site][:url]).host
+        rescue
+          title = ''
+        end
+      end
+
+      if @previous_site.present?
+        @previous_user_site = UserSite.where(site_id: @previous_site.id, user_id: current_user.id)
+        if @previous_user_site.present?
+          render json: {}, status: :conflict
+        else
+          @user_site = UserSite.new(site_id: @previous_site.id, user_id: current_user.id, title: title)
+
+          if @user_site.save
+            render json: @previous_site.as_json.merge({ title: @user_site.title }), status: :created, location: @previous_site
+          else
+            render json: @user_site.errors, status: :unprocessable_entity
+          end
+        end
+      else
+        @site = Site.new(url: params[:site][:url])
+
+        if @site.save
+          @previous_user_site = UserSite.where(site_id: @site.id, user_id: current_user.id)
+          if @previous_user_site.present?
+            render json: @site.as_json.merge({ title: @previous_user_site.title }), status: :created, location: @site
+          else
+            @user_site = UserSite.new(site_id: @site.id, user_id: current_user.id, title: title)
+
+            if @user_site.save
+              render json: @site.as_json.merge({ title: @user_site.title }), status: :created, location: @site
+            else
+              @site.destroy
+
+              render json: @user_site.errors, status: :unprocessable_entity
+            end
+          end
+        else
+          render json: @site.errors, status: :unprocessable_entity
+        end
+      end
     else
-      render json: @site.errors, status: :unprocessable_entity
+      render json: {}, status: :unprocessable_entity
     end
   end
 
   private
     def site_params
-      params.require(:site).permit(:url)
+      params.require(:site).permit(:url, :title)
     end
 end
--- backend/test/controllers/users/current/sites_controller_test.rb
+++ backend/test/controllers/users/current/sites_controller_test.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 require 'test_helper'
 require 'AuthenticationHelper'
 
@@ -6,6 +8,9 @@ class Users::Current::SitesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @user = users(:one)
+    assert_not_empty(@user.sites)
+    @user_no_sites = users(:three)
+    assert_empty(@user_no_sites.sites)
     @user.confirm
 
     @site = sites(:one)
@@ -19,29 +24,140 @@ class Users::Current::SitesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     authentication_get @user, users_current_sites_url, as: :json
     assert_response :success
+  end
 
-    assert_equal(Site.all.to_json, response.body)
+  test "should list no sites for current user with no sites" do
+    authentication_get @user_no_sites, users_current_sites_url, as: :json
+    assert_response :success
+
+    assert_equal([].to_json, response.body)
+  end
+
+  test "should list sites for current user with some subset of sites" do
+    # need more total sites than targeted users sites
+    assert(Site.all.count - UserSite.where(user_id: @user.id).pluck(:site_id).uniq.count > 0)
+
+    authentication_get @user, users_current_sites_url, as: :json
+    assert_response :success
+
+    assert_equal(@user.sites.to_json, response.body)
   end
 
   test "create only available to logged in users" do
     assert_no_difference('Site.count') do
-      post users_current_sites_url, params: { site: { url: 'http://www.newurl.com' } }, as: :json
+      post users_current_sites_url, params: { site: { url: 'http://www.newurl.com', title: 'new' } }, as: :json
     end
     assert_response :unauthorized
   end
 
-  test "should create site" do
-    assert_difference('Site.count') do
-      authentication_post @user, users_current_sites_url, params: { site: { url: 'http://www.newurl.com' } }, as: :json
-    end
-    assert_response 201
-  end
+  test "create with bad site params takes no action" do
+    new_title = 'new'
 
-  test "should not create with non-unique url" do
+    user = @user
+
     assert_no_difference('Site.count') do
-      assert_raises(ActiveRecord::RecordNotUnique) do
-        authentication_post @user, users_current_sites_url, params: { site: { url: @site.url } }, as: :json
+      assert_no_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { random: { url: 'http://www.newurl.com', title: new_title } }, as: :json
       end
     end
+    assert_response :unprocessable_entity
+
+    assert_no_difference('Site.count') do
+      assert_no_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: {} }, as: :json
+      end
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "create with missing title but proper url uses base URL as title" do
+    new_url = 'http://www.newurl.com/'
+
+    user = @user
+
+    assert_difference('Site.count') do
+      assert_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: { url: new_url } }, as: :json
+      end
+    end
+    assert_response :created
+
+    new_user_site = UserSite.last
+    assert_equal(URI(new_url).host, new_user_site.title)
+    assert_equal(user.id, new_user_site.user_id)
+  end
+
+  test "create with improper url and no title" do
+    new_url = true
+
+    user = @user
+
+    assert_difference('Site.count') do
+      assert_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: { url: new_url } }, as: :json
+      end
+    end
+    assert_response :created
+
+    new_user_site = UserSite.last
+    assert_equal('', new_user_site.title)
+    assert_equal(user.id, new_user_site.user_id)
+  end
+
+  test "should create site and user_site for authenticated user with new global site and new for user" do
+    user = @user
+    new_title = 'new'
+    assert_difference('Site.count') do
+      assert_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: { url: 'http://www.newurl.com', title: new_title } }, as: :json
+      end
+    end
+    assert_response :created
+
+    new_user_site = UserSite.last
+    assert_equal(new_title, new_user_site.title)
+    assert_equal(user.id, new_user_site.user_id)
+  end
+
+  test "should only create site user_site for authenticated user with previous global site and no previous user_site for user" do
+    new_url = 'http://www.newurl.com'
+    new_title = 'new'
+
+    previous_site = Site.create(url: new_url)
+
+    user = @user
+
+    assert_no_difference('Site.count') do
+      assert_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: { url: new_url, title: new_title } }, as: :json
+      end
+    end
+    assert_response :created
+
+    new_user_site = UserSite.last
+    assert_equal(new_title, new_user_site.title)
+    assert_equal(user.id, new_user_site.user_id)
+
+    assert_equal(previous_site, previous_site.reload)
+  end
+
+  test "should not create site user_site for authenticated user with previous global site and previous user_site for user" do
+    new_url = 'http://www.newurl.com'
+    new_title = 'new'
+
+    user = @user
+
+    previous_site = Site.create(url: new_url)
+    previous_user_site = UserSite.create(user_id: user.id, site_id: previous_site.id, title: 'random')
+
+    assert_no_difference('Site.count') do
+      assert_no_difference('UserSite.count') do
+        authentication_post user, users_current_sites_url, params: { site: { url: new_url, title: new_title } }, as: :json
+      end
+    end
+    assert_response :conflict
+
+    assert_equal(previous_site, previous_site.reload)
+    assert_equal(previous_user_site, previous_user_site.reload)
   end
 end
