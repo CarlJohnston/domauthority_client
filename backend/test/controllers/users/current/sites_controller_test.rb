b76d16fc43a160afc51c2f7f45f4bfb8b2571229
Carl Johnston <carljohnston28@gmail.com>
2018-06-16 08:51:19 -0700

    Make sites controller tests more granular

--- backend/test/controllers/users/current/sites_controller_test.rb
+++ backend/test/controllers/users/current/sites_controller_test.rb
@@ -12,22 +12,26 @@ class Users::Current::SitesControllerTest < ActionDispatch::IntegrationTest
     @site = sites(:one)
   end
 
-  test "should get index" do
+  test "index should only be available for authenticated users" do
     get users_current_sites_url, as: :json
     assert_response :unauthorized
+  end
 
+  test "should get index" do
     authentication_get @user, users_current_sites_url, as: :json
     assert_response :success
 
     assert_equal(Site.all.to_json, response.body)
   end
 
-  test "should create site" do
+  test "create only available to logged in users" do
     assert_no_difference('Site.count') do
       post users_current_sites_url, params: { site: { url: 'http://www.newurl.com' } }, as: :json
     end
     assert_response :unauthorized
+  end
 
+  test "should create site" do
     assert_difference('Site.count') do
       authentication_post @user, users_current_sites_url, params: { site: { url: 'http://www.newurl.com' } }, as: :json
     end
