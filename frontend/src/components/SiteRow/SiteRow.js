ecbd60e7d9767270579a4223e5fe92b0a4aa3dad
Carl Johnston <carljohnston28@gmail.com>
2018-06-16 17:39:55 -0700

    Add basic Site and SiteRow

---
+++ frontend/src/components/SiteRow/SiteRow.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SiteRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+      </tr>
+    );
+  }
+}
+
+export default SiteRow;
--- frontend/src/components/Sites/Sites.js
+++ frontend/src/components/Sites/Sites.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 
+import SiteRow from 'components/SiteRow/SiteRow';
+
+import Fetch from 'helpers/Fetch';
+import AuthenticationToken from 'helpers/AuthenticationToken';
+
 import withAuthenticated from 'components/hocs/withAuthenticated';
 
 class Sites extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sites: [],
+    };
+  }
+
+  componentDidMount() {
+    var token = AuthenticationToken.get();
+    var request = new Request('/users/current/sites', {
+      'Content-Type': 'application/json',
+    });
+
+    Fetch(request)
+      .then((response) => {
+      })
+      .catch((response) => {
+      });
+  }
+
   render() {
     return (
       <div>
         Sites
+        <table className='hover'>
+          <thead>
+            <tr>
+              <th>Favicon</th>
+              <th>Title</th>
+              <th>URL</th>
+              <th>DA</th>
+              <th>PA</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.sites.map((site) => {
+                return (
+                  <SiteRow site={site} />
+                );
+              })
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
