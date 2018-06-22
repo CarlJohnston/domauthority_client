367c1f8e91f1eff0d1e7841fc97fed236404ed90
Carl Johnston <carljohnston28@gmail.com>
2018-06-21 20:24:16 -0700

    Remove IsAuthenticatedContext from withAuthenticated

--- frontend/src/components/hocs/withAuthenticated.js
+++ frontend/src/components/hocs/withAuthenticated.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
 
-import IsAuthenticatedContext from 'contexts/IsAuthenticatedContext';
-
 import withCurrentUser from 'components/hocs/withCurrentUser';
 import withLoginPopUp from 'components/hocs/withLoginPopUp';
 
 function renderAuthenticatedComponent(AuthenticatedComponent, props, state) {
   if (props.currentUser.username) {
     return (
-      <IsAuthenticatedContext.Provider value={state.isAuthenticated}>
-        <AuthenticatedComponent {...props} />
-      </IsAuthenticatedContext.Provider>
+      <AuthenticatedComponent {...props} />
     );
   } else {
     return (
@@ -22,18 +18,6 @@ function renderAuthenticatedComponent(AuthenticatedComponent, props, state) {
 
 function withAuthenticated(AuthenticatedComponent) {
   return withLoginPopUp(withCurrentUser(class extends Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        isAuthenticated: {
-          setIsAuthenticated: function (isAuthenticated) {
-            this.props.setLoginPopUp(!isAuthenticated);
-          }.bind(this),
-        },
-      };
-    }
-
     render() {
       return renderAuthenticatedComponent(AuthenticatedComponent, this.props, this.state);
     }
