dd6e854c7ced20d605b6465441c5ac568bab335e
Carl Johnston <carljohnston28@gmail.com>
2018-07-20 20:41:05 -0700

    Add Flow to RegisterResponse

--- frontend/src/mixins/RegisterResponse.js
+++ frontend/src/mixins/RegisterResponse.js
@@ -1,10 +1,21 @@
+// @flow
+
 import AuthenticateResponse from 'mixins/AuthenticateResponse';
+
+import type { ParsedResponse } from 'mixins/ParsedResponse.type';
+
+
+type Options = {
+  messages: {
+    success: string,
+  },
+};
 
 /*
  * Helper for registration specific authentication API Responses
  */
 class RegisterResponse extends AuthenticateResponse {
-  constructor(data, options) {
+  constructor(data: ParsedResponse, options: {} = {}) {
     super(data, Object.assign({
       messages: {
         success: 'Successfully registered. Please login using the form below.',
