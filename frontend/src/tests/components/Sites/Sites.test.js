97226ccc23f00157d40bb28f12feae426fa97abb
Carl Johnston <carljohnston28@gmail.com>
2018-07-31 18:59:49 -0700

    Add test for rendering sites properly in Sites component

--- frontend/src/tests/components/Sites/Sites.test.js
+++ frontend/src/tests/components/Sites/Sites.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Sinon from 'sinon';
 import { mount } from 'enzyme';
 import { BeatLoader as Loader } from 'react-spinners';
 
 import Sites from 'components/Sites/Sites';
+import SiteRow from 'components/SiteRow/SiteRow';
 
 describe('Sites', () => {
+  let xhr;
+  let requests = [];
+  beforeAll(() => {
+    xhr = Sinon.useFakeXMLHttpRequest();
+    xhr.onCreate = function (xhr) {
+      requests.push(xhr);
+    };
+  });
+
+  afterAll(() => {
+    xhr.restore();
+  });
+
   let component;
   let createComponent = (props) => {
     if (component) {
@@ -44,5 +59,36 @@ describe('Sites', () => {
         }
       }, 100);
     })).resolves.toBe(true);
+  });
+
+  it('renders sites', () => {
+    createComponent();
+    let sites;
+
+    sites = [
+    ];
+    component.setState({
+      loading: false,
+      sites: sites,
+    });
+    expect(component.find(SiteRow).length).toEqual(0);
+
+    sites = [
+      {
+        title: 'Site 1',
+        url: 'http://www.site1.com',
+      },
+    ];
+    component.setState({
+      loading: false,
+      sites: sites,
+    });
+    expect(component.find(SiteRow).length).toEqual(1);
+    sites.forEach((site) => {
+      expect(component.findWhere((node) => {
+        return node.type() === SiteRow &&
+          node.prop('site') === site;
+      }).length).toEqual(1);
+    });
   });
 });
