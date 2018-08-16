9ab41e7dad60fc8309e71828a1d3eb00d3447e70
Carl Johnston <carljohnston28@gmail.com>
2018-08-16 12:17:25 -0700

    New Sites tests
    What and why:
      => SitesContainer took a lot of the responsibility of Sites
      => Sites now only has responsibility of rendering child SiteRows and passing them the proper props

--- frontend/src/tests/components/Sites/Sites.test.js
+++ frontend/src/tests/components/Sites/Sites.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Sinon from 'sinon';
 import { mount } from 'enzyme';
-import { BeatLoader as Loader } from 'react-spinners';
 
 import SiteRow from 'components/SiteRow/SiteRow';
 import Sites from 'components/Sites/Sites';
@@ -37,39 +36,16 @@ describe('Sites', () => {
     }
   });
 
-  it('shows loader while loading then loads content', async () => {
-    expect.assertions(2);
-
-    createComponent();
-
-    expect(component.find(Loader).exists()).toBe(true);
-
-    component.setState({
-      loading: false,
-    });
-    component.update();
-
-    await expect(new Promise((resolve) => {
-      const intervalId = setInterval(() => {
-        const sites = component.find('#sites');
-        if (sites.exists()) {
-          clearInterval(intervalId);
-
-          resolve(true);
-        }
-      }, 100);
-    })).resolves.toBe(true);
-  });
-
   it('renders sites', () => {
-    createComponent();
+    const onSiteRowRemoveStub = Sinon.stub();
+
     let sites;
 
     sites = [
     ];
-    component.setState({
-      loading: false,
+    createComponent({
       sites: sites,
+      onSiteRowRemove: onSiteRowRemoveStub,
     });
     expect(component.find(SiteRow)).toHaveLength(0);
 
@@ -83,54 +59,17 @@ describe('Sites', () => {
         url: 'http://www.site2.com',
       },
     ];
-    component.setState({
-      loading: false,
+    createComponent({
       sites: sites,
+      onSiteRowRemove: onSiteRowRemoveStub,
     });
-    expect(component.find(SiteRow)).toHaveLength(2);
     sites.forEach((site) => {
       expect(component.findWhere((node) => {
         return node.type() === SiteRow &&
-          node.prop('site') === site;
+          node.prop('site').title === site.title &&
+          node.prop('site').url === site.url &&
+          node.prop('onRemove') === onSiteRowRemoveStub;
       })).toHaveLength(1);
     });
-  });
-
-  it('onSiteRowRemove removes site row from state and removes actual site', async () => {
-    const sites = [
-      {
-        title: 'Site 1',
-        url: 'http://www.site1.com',
-      },
-      {
-        title: 'Site 2',
-        url: 'http://www.site2.com',
-      },
-    ];
-    createComponent();
-    component.setState({
-      sites: sites,
-      loading: false,
-    });
-
-    const siteToRemove = sites[0];
-    const onRemove = component.find(SiteRow).first().prop('onRemove');
-    onRemove(Sinon.stub(), siteToRemove);
-    const request = requests.pop();
-    expect(request).toBeDefined();
-    expect(request.url).toEqual('/users/current/sites');
-    expect(request.method).toEqual('DELETE');
-    request.respond(200);
-    await expect(new Promise((resolve) => {
-      const intervalId = setInterval(() => {
-        component.update();
-        const siteRows = component.find(SiteRow);
-        if (siteRows.length == sites.length - 1) {
-          clearInterval(intervalId);
-
-          resolve(true);
-        }
-      }, 100);
-    })).resolves.toBe(true);
   });
 });
