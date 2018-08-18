6e878c94aa0fe9713107d5dad06328c23327044c
Carl Johnston <carljohnston28@gmail.com>
2018-08-18 15:34:53 -0700

    Fix Profile eslint errors

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -13,9 +13,15 @@ type Props = {
 
 class Profile extends Component<Props> {
   render() {
+    const {
+      currentUser,
+    } = this.props;
+
     return (
       <div>
-        <h1>@{this.props.currentUser.username}</h1>
+        <h1>
+          @{currentUser.username}
+        </h1>
       </div>
     );
   }
