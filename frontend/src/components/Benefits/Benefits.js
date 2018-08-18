424ead7dea6c70857323cbb5d2123f8f4c931722
Carl Johnston <carljohnston28@gmail.com>
2018-08-18 14:46:55 -0700

    Fix Benefit component eslint errors

--- frontend/src/components/Benefits/Benefits.js
+++ frontend/src/components/Benefits/Benefits.js
@@ -10,40 +10,68 @@ class Benefits extends Component<Props> {
     return (
       <div className='grid-x features-grid'>
         <div className='cell'>
-          <h2>Benefits</h2>
+          <h2>
+            Benefits
+          </h2>
         </div>
         <div className='cell'>
-          <p>Setup is painless and offers immediate benefits to both you and your clients.</p>
+          <p>
+            Setup is painless and offers immediate benefits to both you and your clients.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-wrench'></i>
-          <h4>Useful</h4>
-          <p>Track relative to your competitors.</p>
+          <i className='fi-wrench' />
+          <h4>
+            Useful
+          </h4>
+          <p>
+            Track relative to your competitors.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-graph-trend'></i>
-          <h4>Visual</h4>
-          <p>Visualize your metrics at a glance.</p>
+          <i className='fi-graph-trend' />
+          <h4>
+            Visual
+          </h4>
+          <p>
+            Visualize your metrics at a glance.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-calendar'></i>
-          <h4>Automatic</h4>
-          <p>No maintenance on your part.</p>
+          <i className='fi-calendar' />
+          <h4>
+            Automatic
+          </h4>
+          <p>
+            No maintenance on your part.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-mail'></i>
-          <h4>Updates</h4>
-          <p>Summaries straight to your inbox.</p>
+          <i className='fi-mail' />
+          <h4>
+            Updates
+          </h4>
+          <p>
+            Summaries straight to your inbox.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-star'></i>
-          <h4>Simple</h4>
-          <p>Only a URL and name required.</p>
+          <i className='fi-star' />
+          <h4>
+            Simple
+          </h4>
+          <p>
+            Only a URL and name required.
+          </p>
         </div>
         <div className='cell small-6 medium-4'>
-          <i className='fi-dollar'></i>
-          <h4>Free</h4>
-          <p>Tracking without any fees.</p>
+          <i className='fi-dollar' />
+          <h4>
+            Free
+          </h4>
+          <p>
+            Tracking without any fees.
+          </p>
         </div>
       </div>
     );
