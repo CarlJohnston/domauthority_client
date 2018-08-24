5ca4d43ba6a07457acd03766687cda5c87b25dd3
Carl Johnston <carljohnston28@gmail.com>
2018-08-24 15:35:23 -0700

    Add necessary type imports to NotAuthenticatedRoute

--- frontend/src/components/routes/NotAuthenticatedRoute.js
+++ frontend/src/components/routes/NotAuthenticatedRoute.js
@@ -6,6 +6,9 @@ import withCurrentUser from 'components/hocs/withCurrentUser';
 
 import AuthenticatedRouteBase from 'components/routes/AuthenticatedRouteBase';
 
+import type { ComponentType } from 'react';
+import type { CurrentUser as CurrentUserType } from 'contexts/CurrentUserContext.types';
+
 
 type Props = {
   component: ComponentType<{}>,
