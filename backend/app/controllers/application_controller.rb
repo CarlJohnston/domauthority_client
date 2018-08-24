74dae614fc2fa932cd1d9ba6eccd944252f74be3
Carl Johnston <carljohnston28@gmail.com>
2018-08-23 17:28:36 -0700

    Catch RecordNotUnique errors in ApplicationController and respond with :conflict

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::API
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotUnique do |exception|
+    render json: {}, status: :conflict
+  end
+
   protected
 
   def configure_permitted_parameters
