aad493f9d589a7e5e5f6695247f792976b1dc78e
Carl Johnston <carljohnston28@gmail.com>
2018-08-23 17:29:02 -0700

    Use SiteUserSites to de-fatten SitesController

--- backend/app/controllers/users/current/sites_controller.rb
+++ backend/app/controllers/users/current/sites_controller.rb
@@ -15,8 +15,6 @@ class Users::Current::SitesController < ApplicationController
   # POST users/current/sites
   def create
     if params.has_key?(:site) && params[:site].has_key?(:url)
-      @previous_site = Site.find_by(url: params[:site][:url])
-
       if params[:site].has_key?(:title)
         title = params[:site][:title]
       else
@@ -27,40 +25,19 @@ class Users::Current::SitesController < ApplicationController
         end
       end
 
-      if @previous_site.present?
-        @previous_user_site = UserSite.where(site_id: @previous_site.id, user_id: current_user.id)
-        if @previous_user_site.present?
-          render json: {}, status: :conflict
-        else
-          @user_site = UserSite.new(site_id: @previous_site.id, user_id: current_user.id, title: title)
+      @site_user_site = SiteUserSite.new(
+        site_user_site_params.merge(
+          {
+            user_id: current_user.id,
+            title: title,
+          }
+        )
+      )
 
-          if @user_site.save
-            render json: @previous_site.as_json.merge({ title: @user_site.title }), status: :created, location: @previous_site
-          else
-            render json: @user_site.errors, status: :unprocessable_entity
-          end
-        end
+      if @site_user_site.save
+        render json: @site_user_site, status: :created
       else
-        @site = Site.new(url: params[:site][:url])
-
-        if @site.save
-          @previous_user_site = UserSite.where(site_id: @site.id, user_id: current_user.id)
-          if @previous_user_site.present?
-            render json: @site.as_json.merge({ title: @previous_user_site.title }), status: :created, location: @site
-          else
-            @user_site = UserSite.new(site_id: @site.id, user_id: current_user.id, title: title)
-
-            if @user_site.save
-              render json: @site.as_json.merge({ title: @user_site.title }), status: :created, location: @site
-            else
-              @site.destroy
-
-              render json: @user_site.errors, status: :unprocessable_entity
-            end
-          end
-        else
-          render json: @site.errors, status: :unprocessable_entity
-        end
+        render json: @site_user_site.errors, status: :unprocessable_entity
       end
     else
       render json: {}, status: :unprocessable_entity
@@ -82,7 +59,7 @@ class Users::Current::SitesController < ApplicationController
       @site = Site.find(params[:id])
     end
 
-    def site_params
+    def site_user_site_params
       params.require(:site).permit(:url, :title)
     end
 end
