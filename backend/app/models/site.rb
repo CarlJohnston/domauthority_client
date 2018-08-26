93023784188416ea53d0e21031899c8e55fd8378
Carl Johnston <carljohnston28@gmail.com>
2018-08-26 14:24:31 -0700

    Add include option to as_json for Site

--- backend/app/models/site.rb
+++ backend/app/models/site.rb
@@ -1,4 +1,8 @@
 class Site < ApplicationRecord
+  VALID_INCLUDES = [
+    :metrics,
+  ]
+
   has_many :user_sites
   has_many :users, through: :user_sites
   has_many :metrics
@@ -6,10 +10,24 @@ class Site < ApplicationRecord
   attr_accessor :title
 
   def as_json options={}
-    super(options).merge(
-      {
-        title: title,
-      }
-    )
+    additional_fields = {}
+
+    additional_fields.merge!({
+      title: title,
+    })
+
+    if options.has_key?(:include)
+      options[:include].each do |include|
+        if VALID_INCLUDES.include?(include.to_sym)
+          additional_fields.merge!({
+                                     include => self.send(include).as_json,
+                                   })
+        end
+      end
+
+      options.delete(:include)
+    end
+
+    super(options).merge(additional_fields)
   end
 end
