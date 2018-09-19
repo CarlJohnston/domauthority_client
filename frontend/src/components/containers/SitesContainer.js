1a4a5792857c4bf2f84dad131a217fb2115a0ff8
Carl Johnston <carljohnston28@gmail.com>
2018-09-19 16:22:49 -0700

    Resolve SitesType in SitesContainer

--- frontend/src/components/containers/SitesContainer.js
+++ frontend/src/components/containers/SitesContainer.js
@@ -8,6 +8,7 @@ import Sites from 'components/Sites/Sites';
 import Fetcher from 'mixins/Fetcher';
 
 import type { Site as SiteType } from 'components/Sites/Site.type';
+import type { Sites as SitesType } from 'components/Sites/Sites.type';
 import type { onSiteRemove as onSiteRemoveType } from 'components/Sites/onSiteRemove.type';
 import type { onSiteUpdate as onSiteUpdateType } from 'components/Sites/onSiteUpdate.type';
 import type { onSiteCreate as onSiteCreateType } from 'components/Sites/onSiteCreate.type';
